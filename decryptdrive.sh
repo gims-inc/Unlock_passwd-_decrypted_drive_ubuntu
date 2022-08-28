924f8be1bfbe353beb2ee31e3233374504dae2ec
Gabriel Musanga Machisu <83005115+gims-inc@users.noreply.github.com>
2022-08-29 02:24:55 +0300

    Update decryptdrive.sh

--- decryptdrive.sh
+++ decryptdrive.sh
@@ -14,9 +14,7 @@ read -s  password
 dislocker -v -V /dev/$disk -u$password -- /media/bitlocker
 ls /media/bitlocker
 mount -o loop,rw /media/bitlocker/dislocker-file /media/mount
-ls /media/mount
-
-#echo "Your drive is now ready to use!"
+ls /media/mount && echo "Your drive is now ready to use!"
 
 #https://www.baeldung.com/linux/bitlocker-encrypted-device
 #gims-inc
